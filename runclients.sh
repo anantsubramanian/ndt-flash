d99380f2feeae7e0d903490b6b24052e3e36e5ca
Anant Subramanian <anant.subramanian15@gmail.com>
2013-09-05 16:03:36 +0530

    Adding date,time to results

--- runclients.sh
+++ runclients.sh
@@ -2,7 +2,7 @@ if [ -z $5 ]; then
   echo "Usage : sh runclients <java-applet-file> <swf-file> <executable-flash-player-path> <hostname(same-as-swf)> <number-of-tests>"
   exit 0
 fi
-MMCFG_FILE="~/mm.cfg"
+MMCFG_FILE=~/mm.cfg
 if [ ! -e $MMCFG_FILE ]; then
   echo "ErrorReportingEnable=1\nTraceOutputFileEnable=1" > $MMCFG_FILE
 fi
@@ -12,18 +12,22 @@ curdir=$(pwd)
 flashplayer=$3
 hostname=$4
 testnum=$5
+APPLET_RESULTS=appletresults.txt
+FLASH_RESULTS=flashresults.txt
 for (( i=1; i<=$testnum; i++ ))
 do
-  java -jar $applet $hostname >> appletresults.txt &
+  echo "Start Java test $testnum (time $(date -u))" >> APPLET_RESULTS
+  java -jar $applet $hostname >> APPLET_RESULTS &
   javapid=$!
   sleep 90
   kill $javapid
+  echo "Start Flash test $testnum (time $(date -u))" >> FLASH_RESULTS
   cd $flashplayer
   ./flashplayerdebugger $curdir/$swf &
   flashpid=$!
   sleep 90
   kill $flashpid
   cd $curdir
-  cat ~/.macromedia/Flash_Player/Logs/flashlog.txt >> flashresults.txt
+  cat ~/.macromedia/Flash_Player/Logs/flashlog.txt >> FLASH_RESULTS
 done
 echo "Completed"
